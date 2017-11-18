f17c0a10682043f2a2e5f581b423397a2364c2e7
flannj <flannj@gmail.com>
2017-11-18 17:13:25 -0500

    updated server main route

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.set('port', process.env.PORT || 3000);
 app.use(compression());
 
 app.use('/', express.static(__dirname + '/public'));
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+})
 
 // Adding CORS support
 app.all('*', function (req, res, next) {
