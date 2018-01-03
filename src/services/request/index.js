6e97ab9c1fb673e865c6025eb56391f97c625d03
flannj <flannj@gmail.com>
2018-01-03 15:51:57 -0500

    D

--- src/services/request/index.js
+++ src/services/request/index.js
@@ -1,4 +1,6 @@
 export default opts => {
+  console.log(opts);
+
   return new Promise((resolve, reject) => {
     let xhr = new XMLHttpRequest();
     xhr.open(opts.method || "GET", opts.url, true);
