356a6aa9c36fcab59f3204183c63b6182b91e73b
flannj <flannj@gmail.com>
2018-01-03 13:44:54 -0500

    larger url loader limit

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         use: [
-          'url-loader?limit=10000'
+          'url-loader?limit=10000000'
         ]
       }
     ]
